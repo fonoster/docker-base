3aa79c6ad1dbec4c23729f3df2b3537ad46fcdb6
Pedro Sanders <sanderspedro@gmail.com>
2020-12-07 23:43:54 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apk add --update nodejs=10.19.0-r0 npm=10.19.0-r0 ;\
+apk add --update nodejs=12.18.4-r0 npm=12.18.4-r0 ;\
   npm -g install . ; \
   npm rebuild ; \
   rm -rf /var/cache/apk/* /tmp/* /var/tmp/*
