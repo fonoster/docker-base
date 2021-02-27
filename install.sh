9bd4213f369af0956f560522cbbfb48a209b4b54
Pedro Sanders <sanderspedro@gmail.com>
2021-02-27 12:33:03 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apk add --no-cache --update tini nodejs=12.20.1-r0 npm=12.20.1-r0 ;\
+apk add --no-cache --update tini nodejs=12.21.0-r0 npm=12.21.0-r0 ;\
   npm -g install . ; \
   npm rebuild ; \
   rm -rf /var/cache/apk/* /tmp/* /var/tmp/*
